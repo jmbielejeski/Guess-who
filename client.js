0b4c456386805e6278c02dbe86f8c4444bc75d4a
jmbielejeski <jmbielejeski@gmail.com>
2021-02-02 19:49:32 -0700

    checked if the person clicked matches the name of the person

--- client.js
+++ client.js
@@ -2,8 +2,8 @@ function randomNumber(min, max) {
   return Math.floor(Math.random() * (1 + max - min) + min);
 }
 
+//generate a random name to display
 let randomPerson = randomNumber(0, people.length);
-
 function appendName() {
   $('h1').append(`
     ${people[randomPerson].name}
@@ -20,14 +20,27 @@ function onReady() {
   // append to the DOM the peoples images
   for (let i = 0; i < people.length; i++) {
     $('body').append(`
-    <div class="person">
+    <div class="person" data-name="${people[i].name}">
     <img src="https://github.com/${people[i].githubUsername}.png?size=250" alt="Profile image of ${people[i].name}">
     </div>
     `);
   }
   appendName();
+
+  $(document).on('click', '.person', clickedOn);
 }
 
 //function to check if they clicked on right person.
 //how to get right person
-//generate a random name to display
+function clickedOn() {
+  console.log($(this).data('name'));
+  let thisPerson = $(this).data('name');
+  console.log(thisPerson);
+  console.log(people[randomPerson].name);
+  // checking if the person clicked matches the name
+  if (thisPerson === people[randomPerson].name) {
+    console.log('you clicked on the right person');
+  } else {
+    console.log('you clicked on the wrong person');
+  }
+}
